02411c9b5d5cdb509a8e2248af44906a776dd913
Ray Ye <yer@purdue.edu>
2020-06-13 23:45:24 -0400

    Updated README.md and add Key listeners

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # calculator
+
+This project was made using HTML, CSS and vanilla Javascript
+
+__Functions that currently work__:
+- Add, Subtract, Multiply, and Divide Buttons
+- Clear and Equal buttons
+- Number buttons and Number keys
+
+__Things to fix/add__:
+-Add Key listeners for Add, Subtract, Multiply and Divide
+-Add Key listeners for clear
+-Set the max the number can be
+-Be able to reset the number after a number is entered after a number is done calculating
+-Make the number be able to fit for large numbers
+-Add functionality to back space button
+-Make code more efficient and can clean
--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const mulBtn = document.querySelector('#mul')
 const divBtn = document.querySelector('#div')
 const equBtn = document.querySelector('#equal')
 const clearBtn = document.querySelector('#clear')
+const backBtn = document.querySelector('#backspace')
 const val = document.querySelector('#value')
+const decBtn = document.querySelector('#decimal')
 
 //Number Buttons
 const oneBtn = document.querySelector('#one')
@@ -25,6 +27,7 @@ let numStr2 = ""
 let operation = ''
 let ifOperate = false
 let total;
+let numDec = 0
 
 function add(num1, num2) {
   return num1+num2;
@@ -58,40 +61,65 @@ function operate(operator, num1, num2) {
   }
 }
 
-//Button Listeners for +, - , * , /
-addBtn.addEventListener('click', function() {
+function addListen() {
   operation = 'add'
   ifOperate = true
-})
-subBtn.addEventListener('click', function() {
+  numDec = 0
+}
+function subListen() {
   operation = 'subtract'
   ifOperate = true
-})
-mulBtn.addEventListener('click', function() {
+  numDec = 0
+}
+function mulListen() {
   operation = 'multiply'
   ifOperate = true
-})
-divBtn.addEventListener('click', function(){
+  numDec = 0
+}
+function divListen() {
   operation = 'divide'
   ifOperate = true
-})
-equBtn.addEventListener('click', function() {
-  total = operate(operation, parseInt(numStr1), parseInt(numStr2))
-  value.innerHTML = total
+  numDec = 0
+}
+function equalListen() {
+  total = operate(operation, parseFloat(numStr1), parseFloat(numStr1))
+  value.innerHTML = total.toFixed(4)
   numStr1 = "" + total
   numStr2 = ""
-})
-clearBtn.addEventListener('click', function() {
+  numDec = 0
+}
+function clearListen() {
   numStr1 = ""
   numStr2 = ""
   ifOperate = false
   value.innerHTML = 0
-  console.log(numStr1)
-  console.log(numStr2)
-})
+  numDec = 0
+}
+function decListen() {
+  if ((numDec == 0) && (!ifOperate)) {
+    numStr1 += "."
+    value.innerHTML = numStr1
+  }
+  else if ((numDec == 0) && (ifOperate)) {
+    numStr2 += "."
+    value.innerHTML = numStr2
+  }
+  numDec = 1
+}
 
-//Button Listeners for 0,1,2,3,4,5,6,7,8,9
-oneBtn.addEventListener('click', function() {
+
+//Button Listeners for +, - , * , /
+addBtn.addEventListener('click', addListen)
+subBtn.addEventListener('click', subListen)
+mulBtn.addEventListener('click', mulListen)
+divBtn.addEventListener('click', divListen)
+equBtn.addEventListener('click', equalListen)
+clearBtn.addEventListener('click', clearListen)
+decBtn.addEventListener('click', decListen)
+
+
+//Button and Key Listeners for 0,1,2,3,4,5,6,7,8,9
+function oneListen() {
   if (!ifOperate) {
     numStr1 += '1'
     value.innerHTML = numStr1;
@@ -100,22 +128,18 @@ oneBtn.addEventListener('click', function() {
     numStr2 += '1'
     value.innerHTML = numStr2;
   }
-  //console.log(numStr1)
-  //console.log(numStr2)
-})
-
-twoBtn.addEventListener('click', function() {
+}
+function twoListen() {
   if (!ifOperate) {
     numStr1 += '2'
     value.innerHTML = numStr1;
   }
   else {
     numStr2 += '2'
-      value.innerHTML = numStr2;
+    value.innerHTML = numStr2;
   }
-})
-
-threeBtn.addEventListener('click', function() {
+}
+function threeListen() {
   if (!ifOperate) {
     numStr1 += '3'
     value.innerHTML = numStr1;
@@ -124,8 +148,8 @@ threeBtn.addEventListener('click', function() {
     numStr2 += '3'
       value.innerHTML = numStr2;
   }
-})
-fourBtn.addEventListener('click', function() {
+}
+function fourListen() {
   if (!ifOperate) {
     numStr1 += '4'
     value.innerHTML = numStr1;
@@ -134,8 +158,8 @@ fourBtn.addEventListener('click', function() {
     numStr2 += '4'
     value.innerHTML = numStr2;
   }
-})
-fiveBtn.addEventListener('click', function() {
+}
+function fiveListen() {
   if (!ifOperate) {
     numStr1 += '5'
     value.innerHTML = numStr1;
@@ -144,18 +168,18 @@ fiveBtn.addEventListener('click', function() {
     numStr2 += '3'
     value.innerHTML = numStr2;
   }
-})
-sixBtn.addEventListener('click', function() {
-  if (!ifOperate) {
-    numStr1 += '6'
-    value.innerHTML = numStr1;
-  }
-  else {
-    numStr2 += '6'
-    value.innerHTML = numStr2;
-  }
-})
-sevenBtn.addEventListener('click', function() {
+}
+function sixListen() {
+ if (!ifOperate) {
+   numStr1 += '6'
+   value.innerHTML = numStr1;
+ }
+ else {
+   numStr2 += '6'
+   value.innerHTML = numStr2;
+ }
+}
+function sevenListen() {
   if (!ifOperate) {
     numStr1 += '7'
     value.innerHTML = numStr1;
@@ -164,8 +188,8 @@ sevenBtn.addEventListener('click', function() {
     numStr2 += '7'
     value.innerHTML = numStr2;
   }
-})
-eightBtn.addEventListener('click', function() {
+}
+function eightListen() {
   if (!ifOperate) {
     numStr1 += '8'
     value.innerHTML = numStr1;
@@ -174,8 +198,8 @@ eightBtn.addEventListener('click', function() {
     numStr2 += '8'
     value.innerHTML = numStr2;
   }
-})
-nineBtn.addEventListener('click', function() {
+}
+function nineListen() {
   if (!ifOperate) {
     numStr1 += '9'
     value.innerHTML = numStr1;
@@ -184,8 +208,8 @@ nineBtn.addEventListener('click', function() {
     numStr2 += '9'
     value.innerHTML = numStr2;
   }
-})
-zeroBtn.addEventListener('click', function() {
+}
+function zeroListen() {
   if (!ifOperate) {
     numStr1 += '0'
     value.innerHTML = numStr1;
@@ -194,4 +218,52 @@ zeroBtn.addEventListener('click', function() {
     numStr2 += '0'
     value.innerHTML = numStr2;
   }
+}
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode == 13) {
+    equalListen()
+  }
+  else if (e.keyCode == 48) {
+    zeroListen()
+  }
+  else if (e.keyCode == 49) {
+    oneListen()
+  }
+  else if (e.keyCode == 50) {
+    twoListen()
+  }
+  else if (e.keyCode == 51) {
+    threeListen()
+  }
+  else if (e.keyCode == 52) {
+    fourListen()
+  }
+  else if (e.keyCode == 53) {
+    fiveListen()
+  }
+  else if (e.keyCode == 54) {
+    sixListen()
+  }
+  else if (e.keyCode == 55) {
+    sevenListen()
+  }
+  else if (e.keyCode == 56) {
+    eightListen()
+  }
+  else if (e.keyCode == 57) {
+    nineListen()
+  }
+  else if(e.keyCode == 190) {
+    decListen()
+  }
 })
+oneBtn.addEventListener('click', oneListen)
+twoBtn.addEventListener('click', twoListen)
+threeBtn.addEventListener('click', threeListen)
+fourBtn.addEventListener('click', fourListen)
+fiveBtn.addEventListener('click', fiveListen)
+sixBtn.addEventListener('click', sixListen)
+sevenBtn.addEventListener('click', sevenListen)
+eightBtn.addEventListener('click', eightListen)
+nineBtn.addEventListener('click', nineListen)
+zeroBtn.addEventListener('click', zeroListen)
